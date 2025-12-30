6ac3217952c7005fcf68dccf68b09caab7ababa9
rahulxyzten <beherarahul101@gmail.com>
2025-12-30 23:58:42 +0530

    Date- 30/12/25 (Phase 2)

--- src/TakeYouforward/BinarySearch/LeetCode/Hard/P1MedianTwoSortedArrays.java
+++ src/TakeYouforward/BinarySearch/LeetCode/Hard/P1MedianTwoSortedArrays.java
@@ -34,94 +34,132 @@ import java.util.List;
 
 public class P1MedianTwoSortedArrays {
     public static void main(String[] args) {
-        int[] nums1 = {1, 2}, nums2 = {3, 4};
+        int[] nums1 = {1, 2}, nums2 = {3};
         System.out.println(findMedianSortedArrays(nums1, nums2));
     }
 
-    // Optimal Approach (Binary Search) (Mine) (Long Length)
+    // Optimal Approach (Binary Search)
     // T(C) = O(log(min(n1,n2)))
     // S(C) = O(1)
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int n1 = nums1.length, n2 = nums2.length;
-        if (n1 == 0) {
-            if (n2 % 2 == 1) return (double) nums2[n2 / 2];
-            else return (nums2[n2 / 2] + nums2[(n2 / 2) - 1]) / 2.0;
-        }
-        if (n2 == 0) {
-            if (n1 % 2 == 1) return (double) nums1[n1 / 2];
-            else return (nums1[n1 / 2] + nums1[(n1 / 2) - 1]) / 2.0;
-        }
+        if (n2 < n1) return findMedianSortedArrays(nums2, nums1);
+        // We perform the BS on smaller array
+        // See the return statement here
 
+        int start = 0, end = n1;
+        int leftHalf = (n1 + n2 + 1) / 2;
         int n = n1 + n2;
-        int leftHalf;
-        if (n % 2 == 0) {
-            leftHalf = n / 2;
-        } else {
-            leftHalf = (n + 1) / 2;
-        }
-
-        int start = 0, end = Math.min(n1, n2);
-        int l1 = -1, l2 = -1, r1 = -1, r2 = -1;
 
         while (start <= end) {
             int mid1 = start + (end - start) / 2;
             int mid2 = leftHalf - mid1;
 
-            if (n1 <= n2) {
-                if (mid1 == 0) {
-                    l1 = Integer.MIN_VALUE;
-                    r1 = nums1[mid1];
-                } else if (mid1 == n1) {
-                    l1 = nums1[mid1 - 1];
-                    r1 = Integer.MAX_VALUE;
-                } else {
-                    l1 = nums1[mid1 - 1];
-                    r1 = nums1[mid1];
-                }
+            // int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            // int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
 
-                if (mid2 == 0) {
-                    l2 = Integer.MIN_VALUE;
-                    r2 = nums2[mid2];
-                } else if (mid2 == n2) {
-                    l2 = nums2[mid2 - 1];
-                    r2 = Integer.MAX_VALUE;
-                } else {
-                    l2 = nums2[mid2 - 1];
-                    r2 = nums2[mid2];
-                }
-            } else {
-                if (mid1 == 0) {
-                    l1 = Integer.MIN_VALUE;
-                    r1 = nums2[mid1];
-                } else if (mid1 == n2) {
-                    l1 = nums2[mid1 - 1];
-                    r1 = Integer.MAX_VALUE;
-                } else {
-                    l1 = nums2[mid1 - 1];
-                    r1 = nums2[mid1];
-                }
+            // if (mid1 < n1) r1 = nums1[mid1];
+            // if (mid2 < n2) r2 = nums2[mid2];
+            // if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            // if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
 
-                if (mid2 == 0) {
-                    l2 = Integer.MIN_VALUE;
-                    r2 = nums1[mid2];
-                } else if (mid2 == n1) {
-                    l2 = nums1[mid2 - 1];
-                    r2 = Integer.MAX_VALUE;
-                } else {
-                    l2 = nums1[mid2 - 1];
-                    r2 = nums1[mid2];
-                }
-            }
+            int r1 = (mid1 == n1) ? Integer.MAX_VALUE : nums1[mid1];
+            int r2 = (mid2 == n2) ? Integer.MAX_VALUE : nums2[mid2];
+            int l1 = (mid1 == 0) ? Integer.MIN_VALUE : nums1[mid1 - 1];
+            int l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2 - 1];
 
-            if (l1 <= r2 && l2 <= r1) break;
-
-            if (l1 >= r2) end = mid1 - 1;
-            if (l2 >= r1) start = mid1 + 1;
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return (double) Math.max(l1, l2);
+                else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            } else if (l1 > r2) end = mid1 - 1;
+            else start = mid1 + 1; // (l2 > r1)
         }
 
-        if (n % 2 == 1) return (double) Math.max(l1, l2);
-        else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+        return 0.0;
     }
+
+    /* (Mine) (Long Length) */
+//    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//        int n1 = nums1.length, n2 = nums2.length;
+//        if (n1 == 0) {
+//            if (n2 % 2 == 1) return (double) nums2[n2 / 2];
+//            else return (nums2[n2 / 2] + nums2[(n2 / 2) - 1]) / 2.0;
+//        }
+//        if (n2 == 0) {
+//            if (n1 % 2 == 1) return (double) nums1[n1 / 2];
+//            else return (nums1[n1 / 2] + nums1[(n1 / 2) - 1]) / 2.0;
+//        }
+//
+//        int n = n1 + n2;
+//        int leftHalf;
+//        if (n % 2 == 0) {
+//            leftHalf = n / 2;
+//        } else {
+//            leftHalf = (n + 1) / 2;
+//        }
+//
+//        int start = 0, end = Math.min(n1, n2);
+//        int l1 = -1, l2 = -1, r1 = -1, r2 = -1;
+//
+//        while (start <= end) {
+//            int mid1 = start + (end - start) / 2;
+//            int mid2 = leftHalf - mid1;
+//
+//            if (n1 <= n2) {
+//                if (mid1 == 0) {
+//                    l1 = Integer.MIN_VALUE;
+//                    r1 = nums1[mid1];
+//                } else if (mid1 == n1) {
+//                    l1 = nums1[mid1 - 1];
+//                    r1 = Integer.MAX_VALUE;
+//                } else {
+//                    l1 = nums1[mid1 - 1];
+//                    r1 = nums1[mid1];
+//                }
+//
+//                if (mid2 == 0) {
+//                    l2 = Integer.MIN_VALUE;
+//                    r2 = nums2[mid2];
+//                } else if (mid2 == n2) {
+//                    l2 = nums2[mid2 - 1];
+//                    r2 = Integer.MAX_VALUE;
+//                } else {
+//                    l2 = nums2[mid2 - 1];
+//                    r2 = nums2[mid2];
+//                }
+//            } else {
+//                if (mid1 == 0) {
+//                    l1 = Integer.MIN_VALUE;
+//                    r1 = nums2[mid1];
+//                } else if (mid1 == n2) {
+//                    l1 = nums2[mid1 - 1];
+//                    r1 = Integer.MAX_VALUE;
+//                } else {
+//                    l1 = nums2[mid1 - 1];
+//                    r1 = nums2[mid1];
+//                }
+//
+//                if (mid2 == 0) {
+//                    l2 = Integer.MIN_VALUE;
+//                    r2 = nums1[mid2];
+//                } else if (mid2 == n1) {
+//                    l2 = nums1[mid2 - 1];
+//                    r2 = Integer.MAX_VALUE;
+//                } else {
+//                    l2 = nums1[mid2 - 1];
+//                    r2 = nums1[mid2];
+//                }
+//            }
+//
+//            if (l1 <= r2 && l2 <= r1) break;
+//
+//            if (l1 >= r2) end = mid1 - 1;
+//            if (l2 >= r1) start = mid1 + 1;
+//        }
+//
+//        if (n % 2 == 1) return (double) Math.max(l1, l2);
+//        else return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+//    }
 
     // Better Approach
     // T(C) = O(N1+N2)
