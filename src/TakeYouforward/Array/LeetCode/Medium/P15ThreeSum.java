5e40f0e7af8f321faaa12d5b084d6b3853e9494d
rahulxyzten <beherarahul101@gmail.com>
2026-01-23 23:54:07 +0530

    Date- 23/1/26 (Phase 1)

---
+++ src/TakeYouforward/Array/LeetCode/Medium/P15ThreeSum.java
@@ -0,0 +1,69 @@
+package TakeYouforward.Array.LeetCode.Medium;
+// Problem
+/*
+15. 3Sum
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+
+
+Constraints:
+3 <= nums.length <= 3000
+-10^5 <= nums[i] <= 10^5
+ */
+
+import java.util.*;
+
+public class P15ThreeSum {
+    public static void main(String[] args) {
+        int nums[] = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    // T(C) = O(N3 * log(no. of unique triplets)),
+    // inserting triplets into the set takes O(log(no. of unique triplets))
+    // Not considering the time complexity of sorting as we are just sorting 3 elements every time
+    // S(C) =  O(2 * no. of the unique triplets)
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Set<List<Integer>> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(set);
+    }
+}
