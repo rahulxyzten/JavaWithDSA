23e1b96b30235296ce1ce039317b457a7d7946d8
rahulxyzten <beherarahul101@gmail.com>
2026-02-24 20:52:41 +0530

    Date- 24/2/26 (Phase 2)

--- src/KunalKushwaha/CP4Arrays/Leetcode/Medium/P4TwoSumII.java
+++ src/KunalKushwaha/CP4Arrays/Leetcode/Medium/P4TwoSumII.java
@@ -47,14 +47,33 @@ public class P4TwoSumII {
     // Optimal Solution (Two - Pointer)
     // T(C) = O(N)
     // S(C) = O(1)
+//    public int[] twoSum(int[] numbers, int target) {
+//        int n = numbers.length;
+//        int start = 0, end = n - 1, sum;
+//        while (start < end) {
+//            sum = numbers[start] + numbers[end];
+//            if (sum == target) return new int[]{start + 1, end + 1};
+//            else if (sum > target) end--;
+//            else start++;
+//        }
+//
+//        return new int[]{-1, -1};
+//    }
+
+    // Better Approach (Binary Search)
+    // T(C) = O(NlogN)
+    // S(C) = O(1)
     public int[] twoSum(int[] numbers, int target) {
         int n = numbers.length;
-        int start = 0, end = n - 1, sum;
-        while (start < end) {
-            sum = numbers[start] + numbers[end];
-            if (sum == target) return new int[]{start + 1, end + 1};
-            else if (sum > target) end--;
-            else start++;
+        for (int i = 0; i < n; i++) {
+            int start = i + 1, end = n - 1;
+            int rem = target - numbers[i];
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (numbers[mid] == rem) return new int[]{i + 1, mid + 1};
+                else if (numbers[mid] > rem) end = mid - 1;
+                else start = mid + 1;
+            }
         }
 
         return new int[]{-1, -1};
